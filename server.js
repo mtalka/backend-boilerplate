af39a4c9c606d6ec0b2ea4e252bfc2c824542a18
Markus T <markus.talka@gmail.com>
2020-03-02 00:51:18 +0200

    Added cors

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
